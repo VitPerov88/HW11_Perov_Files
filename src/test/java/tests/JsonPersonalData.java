21d028930886b7c2206b7293bfb334342864e722
Vit Perov <vitaliy.perov88@gmail.com>
2023-12-11 22:39:46 +0300

    Third commit

--- src/test/java/tests/JsonPersonalData.java
+++ src/test/java/tests/JsonPersonalData.java
@@ -8,6 +8,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 
+
 public class JsonPersonalData {
     private ClassLoader cl = JsonPersonalData.class.getClassLoader();
 
